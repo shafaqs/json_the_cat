aae86798b8300cbf47d0e1d568300d999bd98aff
Shafaq Saud <shafaq.saud@gmail.com>
2022-10-26 22:56:19 -0400

    refatored chai& mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,19 @@
 const request = require('request');
 
-
-function fetchBreed(breed) {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, function(error, response, body) {
+
     if (error) {
-      console.log(error);
-      return;
+      return callback(error, null);
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      return console.log('Breed not found');
+      return callback("Breed not found");
     }
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    ;
-    // console.log(data);
-    // console.log(typeof data);
-    console.log(breed, ":", data[0].description);
+
+    return callback(null, data[0].description);
   });
-}
+};
 
-fetchBreed(process.argv[2]);
-
+module.exports = { fetchBreedDescription };
